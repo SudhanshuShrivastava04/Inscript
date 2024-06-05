57b4775311344af0fca02558119f25240cd079fc
Sudhanshu Shrivastava <211230053@nitdelhi.ac.in>
2024-06-06 01:59:54 +0530

    diplay notes and pagination

--- server/controllers/dashboard-controller.js
+++ server/controllers/dashboard-controller.js
@@ -1,11 +1,42 @@
+const Note = require("../models/notes.js");
+const mongoose = require("mongoose");
+
 exports.dashboard = async (req, res) => {
-    const locals = {
-      title: "Inscript - Dashboard",
-      description: "Node your notes, script your day.",
-    };
-  
-    res.render("dashboard", {
-      locals,
-      layout: "../views/layouts/dashboard",
-    });
+  let perPage = 12;
+  let page = req.query.page || 1;
+
+  const locals = {
+    title: "Inscript - Dashboard",
+    description: "Node your notes, script your day.",
   };
+
+  try {
+    const notes = await Note.aggregate([
+      { $sort: { updatedAt: -1 } },
+      { $match: { user: new mongoose.Types.ObjectId(req.user.id.toString()) } },
+      {
+        $project: {
+          title: { $substr: ["$title", 0, 30] },
+          body: { $substr: ["$body", 0, 100] },
+        },
+      },
+    ])
+      .skip(perPage * page - perPage)
+      .limit(perPage);
+
+    const count = await Note.countDocuments({
+      user: new mongoose.Types.ObjectId(req.user.id.toString()),
+    });
+
+    res.render("dashboard/index", {
+      userName: req.user.firstName,
+      locals,
+      notes,
+      layout: "../views/layouts/dashboard",
+      current: page,
+      pages: Math.ceil(count / perPage),
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
